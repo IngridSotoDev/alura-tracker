270cb04119b4dc53ac710701aaf4f1b0031fdaff
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-03 15:53:26 -0300

    refact(components, formulario): separacao do cronometro e temporizador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -14,27 +14,7 @@
       </div>
 
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
-
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Temporizador />
       </div>
     </div>
   </div>
@@ -42,31 +22,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
   name: "Formulario",
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    };
-  },
-  computed: {
-    tempoDecorrido(): string {
-      return new Date(this.tempoEmSegundos * 1000)
-        .toISOString()
-        .substring(11, 19);
-    },
-  },
-  methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1;
-      }, 1000);
-    },
-    finalizar() {
-      clearInterval(this.cronometro)
-    },
-  },
+  components: { Temporizador },
 });
 </script>
