0b5ff8a6ef56b434fe5433ca57b27e1637c22b64
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-03 15:17:07 -0300

    feat: add component Formulario

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,22 @@
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <Formulario />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    BarraLateral
-  }
+    BarraLateral,
+    Formulario,
+  },
 });
 </script>
-
-
