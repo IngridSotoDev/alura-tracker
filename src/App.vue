1b54891a4df2f03b20f2981b95f966d83017462a
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-03 16:22:04 -0300

    feat(app): add listagem de tarefas

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
     </div>
     <div class="column is-three-quarter">
       <Formulario />
+
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
@@ -13,12 +18,20 @@
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
 import Formulario from "./components/Formulario.vue";
+import Tarefa from "./components/Tarefa.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
     Formulario,
-  },
+    Tarefa
+},
 });
 </script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+</style>
