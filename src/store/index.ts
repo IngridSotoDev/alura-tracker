98bae481059b2aed71a96eebf07255e50e7844fb
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-04 12:00:51 -0300

    feat(store): cria mutation para alterar projeto

--- src/store/index.ts
+++ src/store/index.ts
@@ -21,6 +21,10 @@ export const store = createStore<State>({
 
       state.projetos.push(projeto);
     },
+    ALTERA_PROJETO(state, projeto: IProjeto) {
+      const index = state.projetos.findIndex(proj => proj.id === projeto.id)
+      state.projetos[index] = projeto
+    }
   },
 });
 
