10f4a770ae145bf61745cd5f0df73f685344cc61
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-04 12:09:56 -0300

    refactor: add rotas aninhadas e separação da listagem de projetos

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,52 +1,15 @@
 <template>
-  <section class="projetos">
+  <div class="projetos">
     <h1 class="title">Projetos</h1>
-
-    <RouterLink to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i>
-      </span>
-      <span>Novo projeto</span>
-    </RouterLink>
-
-    <table class="table is-fullwidth" aria-describedby="Tabela de projetos">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-          <td>
-            <RouterLink :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </RouterLink>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <RouterView></RouterView>
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
-import { useStore } from "@/store";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Projetos",
-  setup() {
-    const store = useStore();
-
-    return {
-      projetos: computed(() => store.state.projetos),
-    };
-  },
 });
 </script>
 
---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <RouterLink to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </RouterLink>
+
+    <table class="table is-fullwidth" aria-describedby="Tabela de projetos">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+          <td>
+            <RouterLink :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </RouterLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
+
+export default defineComponent({
+  name: "Lista",
+  setup() {
+    const store = useStore();
+
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
+  },
+});
+</script>
