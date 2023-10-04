fc039eb11f3e580f751f750375ae6aaf4327687e
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-03 21:06:37 -0300

    feat: adicionar rota de projetos

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router"
 import Tarefas from '../views/Tarefas.vue'
+import Projetos from '../views/Projetos.vue'
+
 
 const routes: RouteRecordRaw[] = [{
   path: '/',
   name: 'Tarefas',
   component: Tarefas
+}, 
+{
+  path: '/projetos',
+  name: 'Projetos',
+  component: Projetos
 }]
 
 const router = createRouter({
