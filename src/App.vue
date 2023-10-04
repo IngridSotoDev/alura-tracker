7780554540d6dd0ed8f47c057d67eb215396d4c0
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-04 13:41:17 -0300

    feat: adicionando componente de notificações

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
+      <Notificacoes />
       <RouterView></RouterView>
     </div>
   </main>
@@ -16,12 +17,14 @@
 import { defineComponent } from "vue";
 
 import BarraLateral from "./components/BarraLateral.vue";
+import Notificacoes from "./components/Notificacoes.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
-  },
+    Notificacoes
+},
   data() {
     return {
       modoEscuroAtivo: false,
---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="notificacoes">
+    <article class="message is-success">
+      <div class="message-header">
+        Atenção
+      </div>
+      <div class="message-body">
+        Aqui vai o texto de notificaçao
+      </div>
+    </article>
+    <article class="message is-warning">
+      <div class="message-header">
+        Atenção
+      </div>
+      <div class="message-body">
+        Aqui vai o texto de notificaçao
+      </div>
+    </article>
+    <article class="message is-danger">
+      <div class="message-header">
+        Atenção
+      </div>
+      <div class="message-body">
+        Aqui vai o texto de notificaçao
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Notificacoes'
+})
+
+</script>
+
+
+<style scoped>
+.notificacoes {
+  z-index: 101;
+  position: absolute;
+  right: 0;
+  width: 300px;
+  padding: 8px;
+}
+
+</style>
