c21e4b8bf80c162495e7dcd702e1b0ca61eb90da
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-04 11:52:44 -0300

    feat(views, Projetos): adicionar botoes de criar e editar projetos

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -2,32 +2,32 @@
   <section class="projetos">
     <h1 class="title">Projetos</h1>
 
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label">Nome do projeto</label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeDoProjeto"
-          id="nomeDoProjeto"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
+    <RouterLink to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </RouterLink>
 
     <table class="table is-fullwidth" aria-describedby="Tabela de projetos">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <RouterLink :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </RouterLink>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -40,23 +40,10 @@ import { useStore } from "@/store";
 
 export default defineComponent({
   name: "Projetos",
-  components: {},
-  data() {
-    return {
-      nomeDoProjeto: "",
-    };
-  },
-  methods: {
-    salvar() {
-      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
-      this.nomeDoProjeto = "";
-    },
-  },
   setup() {
     const store = useStore();
 
     return {
-      store,
       projetos: computed(() => store.state.projetos),
     };
   },
