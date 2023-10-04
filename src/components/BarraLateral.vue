746effdf3537e3bf98d856e54c16b8a44fb8b8d4
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-03 21:12:21 -0300

    feat(components, BarraLateral): add navegação de paginas

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,10 +1,29 @@
 <template>
   <header>
-    <h1>
-      <img src="../assets/logo.png" alt="" />
-    </h1>
+    <div class="has-text-centered">
+      <h1>
+        <img src="../assets/logo.png" alt="" />
+      </h1>
+  
+      <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    </div>
 
-    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <RouterLink to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -41,7 +60,19 @@ header {
   background: #0d3b66;
   width: 100%;
   height: 100vh;
-  text-align: center;
+}
+
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #FAF0CA;
+}
+.link.router-link-active {
+  color: #FAF0CA;
 }
 
 @media only screen and (max-width: 768px) {
