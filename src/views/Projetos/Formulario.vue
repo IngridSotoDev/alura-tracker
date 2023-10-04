b6a8cfebf68a723f42e7248e8dc213dfd2d7f42c
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-04 12:01:20 -0300

    feat(views, Projetos, Formulario): adiciona funcionalidade de edição do projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -16,16 +16,28 @@
         <button class="button" type="submit">Salvar</button>
       </div>
     </form>
-
   </section>
 </template>
 
 <script lang="ts">
-import {  defineComponent } from "vue";
+import { defineComponent } from "vue";
 import { useStore } from "@/store";
 
 export default defineComponent({
   name: "Formulario",
+  props: {
+    id: {
+      type: String,
+    },
+  },
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projetos.find(
+        (proj) => proj.id === this.id
+      );
+      this.nomeDoProjeto = projeto?.nome ?? "";
+    }
+  },
   data() {
     return {
       nomeDoProjeto: "",
@@ -33,9 +45,16 @@ export default defineComponent({
   },
   methods: {
     salvar() {
-      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
-      this.nomeDoProjeto = "";
-      this.$router.push('/projetos')
+      if (this.id) {
+        this.store.commit("ALTERA_PROJETO", {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+        this.nomeDoProjeto = "";
+      }
+      this.$router.push("/projetos");
     },
   },
   setup() {
