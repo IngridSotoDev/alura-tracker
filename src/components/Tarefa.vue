62cff99be5b4352201b4e0758ee0e55fe6bb6019
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-05 18:15:10 -0300

    refact(components, tarefa): migração do methods para o setup

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -2,21 +2,21 @@
   <Box>
     <div class="columns clicavel" @click="tarefaClicada">
       <div class="column is-4">
-        {{ tarefa.descricao || "Tarefa sem descrição" }}
+        {{ tarefaItem.descricao || "Tarefa sem descrição" }}
       </div>
       <div class="column is-3">
-        {{ tarefa.projeto?.nome || "N/D" }}
+        {{ tarefaItem.projeto?.nome || "N/D" }}
       </div>
       <div class="column is-flex is-align-items-center">
         <i class="fa-regular fa-clock mr-2"></i>
-        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+        <Cronometro :tempoEmSegundos="tarefaItem.duracaoEmSegundos" />
       </div>
     </div>
   </Box>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, toRefs } from "vue";
 
 import Box from "./Box.vue";
 import Cronometro from "./Cronometro.vue";
@@ -25,17 +25,24 @@ import ITarefa from "@/interfaces/ITarefa";
 export default defineComponent({
   name: "Tarefa",
   components: { Box, Cronometro },
-  emits: ["onClick"],
+  emits: ["onSelect"],
   props: {
     tarefa: {
       type: Object as PropType<ITarefa>,
       required: true,
     },
   },
-  methods: {
-    tarefaClicada(): void {
-      this.$emit("onClick", this.tarefa);
-    },
+  setup(props, { emit }) {
+    const { tarefa } = toRefs(props);
+
+    const tarefaClicada = (): void => {
+      emit("onSelect", tarefa);
+    };
+
+    return {
+      tarefaItem: tarefa,
+      tarefaClicada,
+    };
   },
 });
 </script>
