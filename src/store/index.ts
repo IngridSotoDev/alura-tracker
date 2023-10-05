128600ec6fc8afe553ca9cece000454d94b702b2
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-05 15:58:21 -0300

    feat(store): criação das mutations e actions de tarefas

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,16 +3,28 @@ import { Store, createStore, useStore as vuexUseStore } from "vuex";
 import IProjeto from "@/interfaces/IProjeto";
 import { INotificacao } from "@/interfaces/INotificacao";
 
-import { EXCLUIR_PROJETO, LISTAR_PROJETOS, NOTIFICAR } from "./mutations-type";
+import {
+  EXCLUIR_PROJETO,
+  EXCLUIR_TAREFA,
+  LISTAR_PROJETOS,
+  LISTAR_TAREFAS,
+  NOTIFICAR,
+} from "./mutations-type";
 import {
   ALTERAR_PROJETO,
+  ALTERAR_TAREFA,
   CADASTRAR_PROJETO,
+  CADASTRAR_TAREFA,
   OBTER_PROJETOS,
+  OBTER_TAREFAS,
   REMOVER_PROJETO,
+  REMOVER_TAREFA,
 } from "./actions-type";
 import http from "@/http";
+import ITarefa from "@/interfaces/ITarefa";
 
 interface State {
+  tarefas: ITarefa[];
   projetos: IProjeto[];
   notificacoes: INotificacao[];
 }
@@ -21,15 +33,22 @@ export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
   state: {
+    tarefas: [],
     projetos: [],
     notificacoes: [],
   },
   mutations: {
-    [EXCLUIR_PROJETO](state, idDoProjeto: number) {
-      state.projetos = state.projetos.filter((proj) => proj.id !== idDoProjeto);
+    [EXCLUIR_PROJETO](state, id: number) {
+      state.projetos = state.projetos.filter((item) => item.id !== id);
     },
     [LISTAR_PROJETOS](state, projetos: IProjeto[]) {
       state.projetos = projetos;
+    },
+    [EXCLUIR_TAREFA](state, id: number) {
+      state.tarefas = state.tarefas.filter((item) => item.id !== id);
+    },
+    [LISTAR_TAREFAS](state, tarefas: ITarefa[]) {
+      state.tarefas = tarefas;
     },
     [NOTIFICAR](state, novaNotificacao: INotificacao) {
       novaNotificacao.id = new Date().getTime();
@@ -56,10 +75,28 @@ export const store = createStore<State>({
     [ALTERAR_PROJETO](context, projeto: IProjeto) {
       return http.put(`projetos/${projeto.id}`, projeto);
     },
-    [REMOVER_PROJETO]({ commit }, idProjeto: number) {
+    [REMOVER_PROJETO]({ commit }, id: number) {
       return http
-        .delete(`projetos/${idProjeto}`)
-        .then(() => commit(EXCLUIR_PROJETO, idProjeto));
+        .delete(`projetos/${id}`)
+        .then(() => commit(EXCLUIR_PROJETO, id));
+    },
+    [OBTER_TAREFAS]({ commit }) {
+      http
+        .get("tarefas")
+        .then((response) => commit(LISTAR_TAREFAS, response.data));
+    },
+    [CADASTRAR_TAREFA](context, nomeDoProjeto: string) {
+      return http.post("tarefas", {
+        nome: nomeDoProjeto,
+      });
+    },
+    [ALTERAR_TAREFA](context, tarefa: ITarefa) {
+      return http.put(`tarefas/${tarefa.id}`, tarefa);
+    },
+    [REMOVER_TAREFA]({ commit }, id: number) {
+      return http
+        .delete(`tarefas/${id}`)
+        .then(() => commit(EXCLUIR_TAREFA, id));
     },
   },
 });
