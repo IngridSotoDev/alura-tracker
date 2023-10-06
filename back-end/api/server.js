a9626e6d289b82de2a76b571ce2af12b7d933e65
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-10-05 22:05:49 -0300

    chore: usa o backend com as mesmas configurações da vercel

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -7,6 +7,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
